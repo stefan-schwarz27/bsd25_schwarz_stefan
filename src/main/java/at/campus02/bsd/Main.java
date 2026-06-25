cbc747c149f4af5ca0d511c95d29baa291bfad60
Stefan Schwarz <s56978@edu.campus02.at>
2026-06-25 06:34:13 +0000

    Integrate Log4j logging

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,13 +1,25 @@
 package at.campus02.bsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
 
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.info("Application started");
+        logger.error("Example error log entry");
+
         Calculator calculator = new Calculator();
+
         System.out.println("Stefan Schwarz");
+
         System.out.println("Add: " + calculator.add(10, 5));
         System.out.println("Subtract: " + calculator.subtract(10, 5));
         System.out.println("Divide: " + calculator.divide(10, 5));
         System.out.println("Multiply: " + calculator.multiply(10, 5));
+        System.out.println("Divide by zero: " + calculator.divide(10, 0));
+	logger.info("Application finished");
     }
 }
